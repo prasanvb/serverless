df86b27dd8e110e1f2cb4345b5f5d21bd72cb2e0
prasanv <prasan.venkatb@gmail.com>
2024-11-25 22:33:24 -0800

    feat: serverless-dynamodb

---
+++ 6-serverless-dynamodb/functions/user/user.js
@@ -0,0 +1,17 @@
+module.exports.handler = async (event, context) => {
+  console.log({ event, context });
+
+  const response = {
+    statusCode: 200,
+    isBase64Encoded: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      message: "Hello from the user function - dynamodb",
+      inputEvent: event,
+    }),
+  };
+
+  return response;
+};
