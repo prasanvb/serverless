09feedc72dab76fd44f5f2bcf4bee0d77adb525d
prasanv <prasan.venkatb@gmail.com>
2024-11-26 22:52:20 -0800

    feat: add role

--- 6-serverless-dynamodb/functions/user/handler.js
+++ 6-serverless-dynamodb/functions/user/handler.js
@@ -1,20 +1,20 @@
-// import { error } from "../utils/response.js";
+import { success, error } from "../utils/response.js";
 import { createUserRecord } from "../utils/dynamodb.js";
 
 export const createUser = async (event, context) => {
   const { USERS_TABLE } = process.env;
-  const body = JSON.parse(event?.body);
+  // const body = JSON.parse(event?.body);
 
   // console.log({ USERS_TABLE, body });
 
   try {
-    const res = await createUserRecord(body, USERS_TABLE);
+    const res = await createUserRecord({}, USERS_TABLE);
 
     console.log("createUserRecord handler response", { res });
 
-    return res;
+    return { ...success, body: JSON.stringify(res) };
   } catch (err) {
     console.error("response error handler", { err });
-    return err;
+    return { ...error, body: JSON.stringify(err) };
   }
 };
