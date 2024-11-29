229eaf1d11f2d304a8a9b15d963369d00814f411
prasanv <prasan.venkatb@gmail.com>
2024-11-28 23:36:35 -0800

    refactor: code clean up

--- 6-serverless-dynamodb/functions/utils/email.js
+++ 6-serverless-dynamodb/functions/utils/email.js
@@ -1,13 +1,10 @@
 import { SESClient, SendRawEmailCommand } from "@aws-sdk/client-ses";
 
-// Initialize SES client
 const sesClient = new SESClient({ region: "us-west-2" });
 
 export const sendRawEmail = async (recipient, subject, bodyText) => {
   const { EMAIL_FROM_ADDRESS } = process.env;
   const sender = EMAIL_FROM_ADDRESS;
-
-  console.log({ sender, recipient, subject, bodyText });
 
   // NOTE: DON'T CHANGE THE FORMAT OF RAW EMAIL STRING
   const rawEmail = `From: ${sender}
@@ -18,11 +15,8 @@ Content-Type: text/plain; charset=UTF-8
 
   ${bodyText}`;
 
-  console.log({ rawEmail });
   // Convert raw email string to a Buffer
   const emailBuffer = Buffer.from(rawEmail);
-
-  // Prepare the command
   const command = new SendRawEmailCommand({
     RawMessage: { Data: emailBuffer },
   });
